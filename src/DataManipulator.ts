ea04a1b0fb2ee40ea63797ae34c9fc3de15e3fe8
sint18 <htoohtoo.slh@gmail.com>
2024-07-21 23:55:41 +0100

    feat: added upper/lower bound, ratio and trigger alert

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const price1 = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const price2 = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = price1 / price2
+    const upper_bound = 1 + 0.05
+    const lower_bound = 1 - 0.05
+    const trigger = (ratio > upper_bound || ratio < lower_bound) ? ratio : undefined
+    return {
+      price_abc: price1,
+      price_def: price2,
+      ratio: ratio,
+      upper_bound: upper_bound,
+      lower_bound: lower_bound,
+      trigger_alert: trigger,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+    }
   }
 }
